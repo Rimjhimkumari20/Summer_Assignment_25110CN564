edbaa08a325f46a18ab4cef29d1e3bcc6cc30ce2
Rimjhimkumari20 <rimjhimkumari0204@gmail.com>
2026-06-10 18:13:43 +0530

    DAY-10 uploaded

---
+++ DAY-10/Q4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    for(int i=1;i<=5;i++){
+        char ch='A';
+        for(int j=1;j<=5-i;j++){
+            printf(" ");
+        }
+       for(int j=1;j<=i;j++){
+         printf("%c",ch);
+         ch++;
+     }
+     
+    ch-=2;
+     for(int k=i-1;k>=1;k--){
+       printf("%c",ch);
+        ch--;
+       }
+  printf("\n");
+    }
+ return 0;
+}
